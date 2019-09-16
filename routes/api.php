10554ab50dd56a3b070bab87dc1dc9184af88e3b
Mauricio Yochizumi <mauricioyochizumi@MacBook-Pro-de-Mauricio.local>
2019-09-16 16:50:43 -0300

    adding login, register and logout routes

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,12 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/login', 'AuthController@login');
+Route::post('/register', 'AuthController@register');
+Route::middleware('auth:api')->post('/logout', 'AuthController@logout');
+
+
+
 Route::get('/foo', function () {
     return 'bar';
 });
